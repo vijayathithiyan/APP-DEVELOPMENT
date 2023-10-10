bf5b6b9e2d30af1bbdfeff034c8214f41bb49c71
vijayathithiyan <144885363+vijayathithiyan@users.noreply.github.com>
2023-10-10 12:20:10 +0530

    Add files via upload

---
+++ DAY-13/src/main/java/com/ravenclass/vijayathithiyan/VijayathithiyanApplication.java
@@ -0,0 +1,13 @@
+package com.ravenclass.vijayathithiyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VijayathithiyanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VijayathithiyanApplication.class, args);
+	}
+
+}
