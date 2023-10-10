ce29e7ce77ca5b505b20856ebec3465d2aa79b77
vijayathithiyan <144885363+vijayathithiyan@users.noreply.github.com>
2023-10-10 12:18:35 +0530

    Add files via upload

---
+++ DAY-11/src/test/java/com/ravenclass/vijayathithiyan/VijayathithiyanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravenclass.vijayathithiyan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VijayathithiyanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
